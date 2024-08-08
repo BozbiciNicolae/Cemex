983a5e5f3b2bdb3c3b0cbd5e2c56e0d0ef54b378
Nicolae Bozbici <nicolae.bzb@gmail.com>
2024-08-08 14:41:11 +0300

    Orders table UI

--- src/app/routes/OrderHistory/orders/orders.component.ts
+++ src/app/routes/OrderHistory/orders/orders.component.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import Order from '../../../types/order';
 import {MatTableModule} from '@angular/material/table';
 
+
 @Component({
   selector: 'orders',
   templateUrl: 'orders.component.html',
   standalone: true,
-  imports: [CommonModule, MatTableModule]
+  imports: [CommonModule, MatTableModule],
+  styleUrl: 'orders.component.less'
 })
 
 export class OrdersComponent {
   @Input() items: Order[] = [];
+
+  displayedColumns: string[] = ['status', 'orderNumber', 'productLine', 'productName', 'quantity', 'createdAt'];
 }
